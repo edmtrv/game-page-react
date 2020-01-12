d0f6c5f308e05dbd2ea57b06eb839f85b6f5ede1
Emil Dimitrov <edimitrov1@gmail.com>
2020-01-12 10:35:31 +0200

    Set inital state of the app

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,18 @@ import Content from './Content';
 import './App.css';
 
 class App extends React.Component {
+  state = { text: '' };
+
+  componentDidMount() {
+    this.setState({ text: this.props.content.description });
+  }
+
   render() {
     return (
       <div className="ui container">
         <Buttons />
         <div className="ui segment">
-          <Content />
+          <Content content={this.state.text} />
         </div>
       </div>
     );
