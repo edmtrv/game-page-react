fe2870df1690db33d6acd33aaaee8b58007c97d4
Emil Dimitrov <edimitrov1@gmail.com>
2020-01-21 02:18:48 +0200

    Updates

---
+++ precache-manifest.fb7f8b51270c40b39f4b1c1def89597c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feeb9d1f22d38df2c53fb1876009cb8c",
+    "url": "/game-page-react/index.html"
+  },
+  {
+    "revision": "b5265cd5597c99d4b6b7",
+    "url": "/game-page-react/static/css/main.8a53747b.chunk.css"
+  },
+  {
+    "revision": "28651c14d6a47871fc27",
+    "url": "/game-page-react/static/js/2.1a9ac0e9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/game-page-react/static/js/2.1a9ac0e9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b5265cd5597c99d4b6b7",
+    "url": "/game-page-react/static/js/main.9cbc9c96.chunk.js"
+  },
+  {
+    "revision": "55d68c45815712010ab6",
+    "url": "/game-page-react/static/js/runtime-main.b8d8388f.js"
+  }
+]);
